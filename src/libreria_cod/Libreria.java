fe158d9135f6b71d9c4c99809b74b278d3b5cabf
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2018-01-19 09:39:18 +0100

    metodo que muestra un mensaje con dos opciones como respuesta

--- src/libreria_cod/Libreria.java
+++ src/libreria_cod/Libreria.java
@@ -1,7 +1,13 @@
-
 package libreria_cod;
+
+import javax.swing.JOptionPane;
 
 
 public class Libreria {
-    
+
+    public static int hacerPregunta() {
+
+        int opcion = JOptionPane.showConfirmDialog(null, "¿Desea mas informacion?","pregunta",JOptionPane.YES_NO_OPTION);
+        return opcion;
+    }
 }
